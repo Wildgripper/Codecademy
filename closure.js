dfe150772b19b6c7ff28bdf9f15a5b4894854a73
Petar Manoilov <manoilov@gmail.com>
2022-01-29 23:41:03 +0200

    partial application

--- closure.js
+++ closure.js
@@ -1,13 +1,9 @@
-function createCounter() {
-  let counter = 0
-  const myFunction = function () {
-    counter = counter + 1
-    return counter
+let c = 4
+function addX(x) {
+  return function (n) {
+    return n + x
   }
-  return myFunction
 }
-const increment = createCounter()
-const c1 = increment()
-const c2 = increment()
-const c3 = increment()
-console.log('example increment', c1, c2, c3)
+const addThree = addX(3)
+let d = addThree(c)
+console.log('example partial application', d)
