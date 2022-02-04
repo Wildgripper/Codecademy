61986bac8c1b4320263c74358da08029e2380fdf
Petar Manoilov <manoilov@gmail.com>
2022-02-04 23:26:24 +0200

    Functions

--- classes/functions.js
+++ classes/functions.js
@@ -6,5 +6,17 @@ function myFunction (a, b) {
     return sum(a, b);
 }
 
+// let x = myFunction(5, 10);
+// console.log(x);
 
+let number = 0;
 
+const add = function(num) {
+    for (let index = 0; index <= num; index++) {
+        number = number + index;        
+    }
+    console.log(number);
+}
+
+add(5);
+add(5);
