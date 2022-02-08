d635f88838212d95f7f0de0ab585e9bac5c11085
Petar Manoilov <manoilov@gmail.com>
2022-02-08 23:47:44 +0200

    In operator

--- classes/in.js
+++ classes/in.js
@@ -1,14 +1,20 @@
-const car = {
+// Arrays
+let trees = ['redwood', 'bay', 'cedar', 'oak', 'maple']
+0 in trees // returns true
+3 in trees // returns true
+6 in trees // returns false
+'bay' in trees // returns false (you must specify the index number, not the value at that index)
+    'length' in trees // returns true (length is an Array property)
+Symbol.iterator in trees // returns true (arrays are iterable, works only in ES2015+)
+
+// Predefined objects
+'PI' in Math // returns true
+
+// Custom objects
+let mycar = {
     make: 'Honda',
     model: 'Accord',
     year: 1998
-};
-
-console.log('make' in car);
-
-delete car.make;
-if ('make' in car === false) {
-    car.make = 'Suzuki';
 }
-
-console.log(car.make);
+'make' in mycar // returns true
+    'model' in mycar // returns true
