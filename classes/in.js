bb922237e2faf02bece2d6013ba9fd0917100b6a
Petar Manoilov <manoilov@gmail.com>
2022-02-15 22:58:52 +0200

    Pi

--- classes/in.js
+++ classes/in.js
@@ -16,8 +16,11 @@ console.log(car.make);
 
 let trees = ['redwood', 'bay', 'cedar', 'oak', 'maple'];
 
-console.log(0 in trees);
-console.log(3 in trees);
-console.log(6 in trees);
-console.log('bay' in trees);
-console.log('length' in trees);
+// console.log(0 in trees);
+// console.log(3 in trees);
+// console.log(6 in trees);
+// console.log('bay' in trees);
+// console.log('length' in trees);
+console.log('PI' in Math);
+
+
