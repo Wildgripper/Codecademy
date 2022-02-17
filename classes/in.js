5bef6415997f9f59a2304d2314651dc6ade6b88a
Petar Manoilov <manoilov@gmail.com>
2022-02-17 23:11:06 +0200

    Undefined

--- classes/in.js
+++ classes/in.js
@@ -1,26 +1,5 @@
-const car = {
-    make: 'Honda',
-    model: 'Accord',
-    year: 1998
-};
-
-console.log('make' in car);
-
-delete car.make;
-
-if ('make' in car === false) {
-    car.make = 'Suzuki';
-}
-
-console.log(car.make);
-
 let trees = ['redwood', 'bay', 'cedar', 'oak', 'maple'];
+trees[3] = undefined;
 
-// console.log(0 in trees);
-// console.log(3 in trees);
-// console.log(6 in trees);
-// console.log('bay' in trees);
-// console.log('length' in trees);
-console.log('PI' in Math);
-
-
+console.log(trees);
+console.log(3 in trees);
